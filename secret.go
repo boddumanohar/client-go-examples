9c2dcf34f2dd9f4c94e89096d257853e8339d109
Manohar Reddy <b.manu199@gmail.com>
2020-10-25 17:09:32 +0530

    init

---
+++ secret.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	coreV1Types "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
+)
+
+// API client for managing secrets
+var secretsClient coreV1Types.SecretInterface
+
+func initClient() {
+	kubeconfig := os.Getenv("HOME") + "/.kube/config"
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	secretsClient = clientset.CoreV1().Secrets("default")
+}
+
+func main() {
+	initClient()
+	secret := &v1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: "default",
+			Name:      "my-secret",
+		},
+		Data: map[string][]byte{
+			"secret-data": []byte("secret-value-1"),
+		},
+	}
+	_, err := secretsClient.Create(context.TODO(), secret, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+}
+
